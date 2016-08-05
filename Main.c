3f1638292edf0e38a3a9d3a01645e2a9cd41e9fe
SergioCubur <sergiocubur@gmail.com>
2016-08-05 17:01:19 -0600

    Ingreso de Comandos

--- Main.c
+++ Main.c
@@ -1,40 +1,53 @@
-#include <stdio.h>
-int main(int argc, char *argv[]) {
-        
-	int opcion;
-	printf("Ingrese un comando");
-	scanf("%d",&opcion);
-	switch(opcion)
-	{
-		case 1:
-			printf("uno\n");
-			break;
-		case 2:
-			printf("dos\n");
-			break;
-		case 3:
-			printf("tres\n");
-			break;
-		case 4:
-			printf("cuatro\n");
-			break;
-		case 5:
-			printf("cinco\n");
-			break;
-		case 6:
-			printf("seis\n");
-			break;
-		case 7:
-			printf("siete\n");
-			break;
-		case 8:
-			printf("ocho\n");
-			break;
-		case 9:
-			printf("nueve\n");
-			break;
-		default:
-			printf("Default\n");
-			break;
-	}
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define BUFFERTAM 150
+void main()
+{
+    menu();
+    return 0;
+}
+
+void menu()
+{
+    char comando[]=" ";
+
+    printf("root@201314292$ ");
+    char buffer[BUFFERTAM];
+
+    while(fgets(comando,BUFFERTAM,stdin) != NULL)
+    {
+        lexico(comando);
+        printf("root@201314292$ ");
+    }
+}
+
+void lexico(char cadena[])
+{
+    char espacio[] = " ";
+
+    char *tocken = NULL;
+
+    tocken = strtok( cadena, espacio);
+
+    while( tocken != NULL ) {
+        printf( "Tocken : %s \n", tocken);
+
+        tocken = strtok( NULL, espacio);
+    }
+
+
+
+/*
+    switch(letra)
+    {
+        case 'a':printf("aaa");break;
+        case 'b':printf("bbb");break;
+        case 'c':printf("ccc");break;
+        case 'd':printf("ddd");break;
+        case 'e':printf("eee");break;
+        default:printf("Def");break;
+
+    }*/
 }
